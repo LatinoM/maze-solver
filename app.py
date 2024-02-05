c09794b2a79a2c0037eff8b85775329584be71dd
LatinoM <latinom@cardiff.ac.uk>
2024-02-05 01:12:07 +0000

    added Point and Line class

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from window import Window
+from line import Line
+from point import Point
 
 def main():
     win = Window(800, 600)
+    p1 = Point(100, 60)
+    p2 = Point(650, 450)
+    l1 = Line(p1, p2)
+    win.draw_line(l1, "green")
     win.wait_for_close()
 
 if __name__ == "__main__":
---
+++ point.py
@@ -0,0 +1,10 @@
+class Point():
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def x(self):
+        return self.__x
+
+    def y(self):
+        return self.__y
